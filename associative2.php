f80bcb572f78e434057a73cc4eaf4fa79b9e08cb
pondokit_zaqi <zaqialhaq.88@gmail.com>
2017-09-06 11:47:41 +0700

    saya

---
+++ associative2.php
@@ -0,0 +1,10 @@
+<?php
+
+$umur = array(
+	"Zaki"=>"15",
+	"Usman"=>"20",
+	"Farid"=>"22"
+	);
+	
+echo "Umur Zaki " . $umur['Zaki'] . " tahun.";
+?>
---
+++ associative3.php
@@ -0,0 +1,5 @@
+<?php 
+
+$handphone= array("Asus"=> "dell", "Vivo"=>"RE34", "Nokia"=>"E63");
+echo "merk laptop " . $handphone["Asus"];
+?>
